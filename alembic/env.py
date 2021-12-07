1601a086f0e0c53d8f7c6784d14784321654a6bb
Hoa Pham <31373574+hlpd-pham@users.noreply.github.com>
2021-12-07 10:42:16 -0600

    feat(db): add sqlalchemy configs

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,11 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
+from sqlalchemy.engine import create_engine
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -25,6 +27,10 @@ target_metadata = None
 # ... etc.
 
 
+def get_url():
+    return f"psql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost/{os.getenv('POSTGRES_DB')}"
+
+
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
@@ -37,7 +43,7 @@ def run_migrations_offline():
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -56,12 +62,7 @@ def run_migrations_online():
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
+    connectable = create_engine(get_url())
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
