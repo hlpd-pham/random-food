698c0511f1a52311f7f4c25466e0f64507dee930
Hoa Pham <31373574+hlpd-pham@users.noreply.github.com>
2021-12-08 14:03:26 -0600

    screw python

--- app/read_recipes.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('assets/spoonacular.json') as f:
-    data = json.load(f)
-
-    for item in data.get('menuItems', None):
-        print(item)
-        print('-----------------')
-        # print(f'{item.get("title", None)}')
