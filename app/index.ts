50adffe7afac5ac321bb93acf6bf4d36d4ffd3df
Hoa Pham <31373574+hlpd-pham@users.noreply.github.com>
2021-12-08 19:59:36 -0600

    downgrade chalk to get it to work

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,13 @@
 import { MenuItem } from './interfaces/menu-item';
 import figlet from 'figlet';
+import chalk from 'chalk';
 import { getMenuItems } from './data/menu-item-data';
 
-console.log(figlet.textSync('Random Recipe', { verticalLayout: 'full' }));
+console.log(chalk['red'](figlet.textSync('Random Recipe', { verticalLayout: 'full' })));
 
 const menuItems: MenuItem[] = getMenuItems();
 const randomItem: MenuItem = menuItems[Math.floor(Math.random() * menuItems.length)];
 
-console.log('--------------------------');
-console.log('Today you should eat');
-console.log(randomItem.title);
-console.log('This is how it looks', randomItem.imageUrl);
+console.log(chalk['yellow']('-----------------------------------------------------'));
+console.log('Today you should eat:', randomItem.title);
+console.log('This is how it looks:', randomItem.imageUrl);
